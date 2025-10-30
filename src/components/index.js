f35161a6216776f7eeb224d0cfda3a4bbe749f5d
Abhishek Sharma <abhisheksharma62263@gmail.com>
2025-10-30 21:53:05 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
-import Login from "./LogIn";
+import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
